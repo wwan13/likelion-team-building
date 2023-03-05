a98fd3c5176cd7595a5ee21860993e34151abbdc
wwan13 <wwan13@naver.com>
2023-03-05 22:22:17 +0900

    [feat]: flip effect

--- src/TeamCard/TeamCard.js
+++ src/TeamCard/TeamCard.js
@@ -2,21 +2,36 @@ import './TeamCard.css'
 
 function TeamCard(props) {
 
-    return (
-        <div className='TeamCard'>
-            <div className='LeaderSection'>
-                <p className='TeamNumber'># team{props.teamNum}</p>
-                <div className='LeaderTextSection'>
-                    <p className='Leader'>Leader</p>
-                    <p className='TeamLeader'>{props.teamLeader}</p>
-                </div>
-            </div>
+    let plip = (event) => {
+        let elem = event.currentTarget;
+        if (elem.style.transform == "rotateY(180deg)") {
+            elem.style.transform = "rotateY(0deg)";
+        } else {
+            elem.style.transform = "rotateY(180deg)";
+        }
+    }
 
-            <div className='MemberSection'>
-                <p className='TeamMember'>{props.mem1}</p>
-                <p className='TeamMember'>{props.mem2}</p>
-                <p className='TeamMember'>{props.mem3}</p>
-                <p className='TeamMember'>{props.mem4}</p>
+    return (
+        <div className='TeamCard' onClick={plip}>
+
+            <div className='TeamCardBack'>
+                # TEAM{props.teamNum} 
+            </div>
+            <div className='TeamCardFront'>
+                <div className='LeaderSection'>
+                    <p className='TeamNumber'># team{props.teamNum}</p>
+                    <div className='LeaderTextSection'>
+                        <p className='Leader'>Leader</p>
+                        <p className='TeamLeader'>{props.teamLeader}</p>
+                    </div>
+                </div>
+
+                <div className='MemberSection'>
+                    <p className='TeamMember'>{props.mem1}</p>
+                    <p className='TeamMember'>{props.mem2}</p>
+                    <p className='TeamMember'>{props.mem3}</p>
+                    <p className='TeamMember'>{props.mem4}</p>
+                </div>
             </div>
         </div>
     )
